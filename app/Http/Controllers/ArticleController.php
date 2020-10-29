487398a93124c544f7774f1e666ecf9e0c44936a
zoran <zoran_gogusev@yahoo.com>
2020-10-29 14:34:54 +0100

    Display articles and create pagination

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -15,7 +15,7 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::paginate(15);
+        $articles = Article::orderBy('created_at', 'desc')->paginate(3);
 
         return ArticleResource::collection($articles);
     }
