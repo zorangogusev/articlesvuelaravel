b4349d15a6ef470c64b15420a1039277fd371464
zoran <zoran_gogusev@yahoo.com>
2020-11-12 23:25:29 +0100

    Heroku

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,8 +16,8 @@ class AuthController extends Controller
             $response = $http->post(config('services.passport.login_endpoint'), [
                 'form_params' => [
                     'grant_type' => 'password',
-                    'client_id' => '2',
-                    'client_secret' => 'kHC0vEGoEQLfZsIHgm4riYi3pxbRuEDikJ9PqE5F',
+                    'client_id' => config('services.passport.client_id'),
+                    'client_secret' => config('services.passport.client_secret'),
                     'username' => $request->username,
                     'password' => $request->password,
                 ]
