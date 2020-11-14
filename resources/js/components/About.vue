a65bc37eda7e6096cae26151c0921dc489d03c80
zoran <zoran_gogusev@yahoo.com>
2020-11-14 16:12:55 +0100

    About.vue page

--- resources/js/components/About.vue
+++ resources/js/components/About.vue
@@ -5,7 +5,7 @@
             This app is using the following tehnologies<br/>
             Docker, PHP7.3, Maria-DB10.1, Laravel8.0, Vue.js, Vuex, Passport, BootstrapVue, VeeVaildate, AnimateCss, cxlt-vue2-toastr
         </p>
-        <p>You can register in the Register page, and after login in the Login page, and can create, update, delete articles that are going to be displayed on the home page</p>
+        <p>You can register in the Register page, after login in the Login page, and you can create, update, delete articles that are going to be displayed on the home page</p>
     </div>
 </template>
 
